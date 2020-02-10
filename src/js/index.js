f8fec6c5adb9c782472a4c57cbe3af7bd56f1764
Alex Leonov <spawnleon@gmail.com>
2020-02-10 17:52:35 +0300

    sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
 import Swiper from 'swiper';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new Swiper('.main-slider__container', {
     loop: true,
   });
   const catalogSlider = new Swiper('.catalog-slider__container', {
     loop: true,
+  });
+  const bottomSlider = new Swiper('.bottom-slider__container', {
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      prevEl: '.bottom-slider__button--prev',
+      nextEl: '.bottom-slider__button--next',
+    },
   });
 });
