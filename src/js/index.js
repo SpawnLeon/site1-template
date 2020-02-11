d1c18a66be8ddf7f9d5cc2da925d146e95d68e30
Alex Leonov <spawnleon@gmail.com>
2020-02-11 15:30:12 +0300

    start detail card

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 import Swiper from 'swiper';
 
+const responseMenu = (menuSelector) => {
+  document.querySelectorAll(menuSelector).forEach((menu) => {
+    const menuChildren = menu.children;
+    const hiddenMenuItems = document.createElement('LI');
+    hiddenMenuItems.classList.add('hidden-menu-items');
+    hiddenMenuItems.innerHTML =
+      '<button class="hidden-menu-items__btn btn--clear"><span></span><span></span><span></span></button>';
+    const hiddenMenuItemsList = document.createElement('UL');
+    hiddenMenuItemsList.classList.add('hidden-menu');
+    hiddenMenuItems.appendChild(hiddenMenuItemsList);
+    const menuWidth = menu.offsetWidth;
+
+    let menuItemsWidth = 27;
+
+    [...menuChildren].forEach((menuItem) => {
+      menuItemsWidth += menuItem.offsetWidth;
+
+      if (menuItemsWidth > menuWidth) {        
+        hiddenMenuItemsList.appendChild(menuItem);
+      }
+    });
+
+    menu.appendChild(hiddenMenuItems);
+  });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new Swiper('.main-slider__container', {
     loop: true,
@@ -15,4 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
       nextEl: '.bottom-slider__button--next',
     },
   });
+  responseMenu('.catalog-menu');
 });
